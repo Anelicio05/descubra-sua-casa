d72bd5b2052edbaef3b709ddca36b3f9eb7d7b21
Anelicio Santos <an.eli.cio05@gmail.com>
2023-02-10 20:41:48 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,79 @@
+import quiz from "./questoes.js";
+const div = document.getElementById("pergutas")
+
+const casasImagem = document.querySelectorAll(".img-casas")
+
+let armazenaResposta = []
+
+function selecionaCasa(resposta) {
+    let resultado
+    for (let i = 0; i < quiz.length; i++) {
+        const questoes = quiz[i].respostas
+        questoes.forEach((e) => {
+            if (e.opcao === resposta) {
+                resultado = e.casa
+            }
+        })
+    }
+
+    armazenaResposta.push(resultado)
+
+    return resultado
+}
+
+let i = 0 
+function mostraPergunta() {
+    const questao = quiz[i].questao
+    const resposta = quiz[i].respostas
+
+    div.innerHTML = ""
+    resposta.forEach((r) => {
+        div.innerHTML += `
+                <button class="respostas">${r.opcao}</button>
+            `
+    })
+
+    const botoes = div.querySelectorAll(".respostas");
+    botoes.forEach((botao) => {
+        botao.addEventListener("click", (b) => {
+            selecionaCasa(b.target.innerHTML);
+            
+            i += 1
+            if (i < quiz.length) {
+                mostraPergunta()
+            }else {
+                casaEscolhida(armazenaResposta)
+                div.style.display = "none"
+            }
+        })
+    })
+}
+    
+function casaEscolhida(casa) {
+    let contagem = casa.reduce((contagem, palavra) => {
+        contagem[palavra] = (contagem[palavra] || 0) + 1
+        return contagem
+    }, {})
+    let palavraMaisFrequente = ''
+    let contagemMaxima = 0
+
+    for (let palavra in contagem) {
+        if (contagem[palavra] > contagemMaxima) {
+            contagemMaxima = contagem[palavra]
+            palavraMaisFrequente = palavra
+        }
+    }
+
+    casasImagem.forEach((img)=>{
+        if(palavraMaisFrequente === img.id){
+           const teste = document.getElementById(`${img.id}`).style.display = "flex"
+           console.log(teste)
+        }
+    })
+
+    
+
+    return palavraMaisFrequente
+}
+
+mostraPergunta()
---
+++ script/questoes.js
@@ -0,0 +1,31 @@
+const quiz = [
+    {
+        questao: "você é uma pessoa ?",
+        respostas:[
+            {opcao: "aventureiros", casa: "Grifinória"},
+            {opcao: "pacientes", casa: "Lufa-Lufa"},
+            {opcao: "inteligência", casa: "Corvinal"},
+            {opcao: "ambição", casa: "Sonserina"}
+        ]
+    },
+    {
+        questao: "sua melhor qualidade ?",
+        respostas:[
+            {opcao: "dedicado", casa: "Lufa-Lufa"},
+            {opcao: "cavalheiro", casa: "Grifinória"},
+            {opcao: "grandeza", casa: "Sonserina"},
+            {opcao: "criatividade", casa: "Corvinal"} 
+        ]
+    },
+    {
+        questao: "você é ?",
+        respostas:[
+            {opcao: "líder", casa: "Sonserina"}, //Corvinal Grifinória Lufa-Lufa Sonserina
+            {opcao: "excentricidade", casa: "Corvinal"},
+            {opcao: "ingênuos", casa: "Lufa-Lufa"},
+            {opcao: "coragem", casa: "Grifinória"} 
+        ]
+    }
+]
+
+export default quiz
